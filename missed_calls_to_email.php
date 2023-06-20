ec1b17e6f40fc8d6ade3ae2a4b4021a0a576f3b9
SBZ systems <support@sbzsystems.com>
2023-06-20 12:36:58 +0300

    Update missed_calls_to_email.php

--- missed_calls_to_email.php
+++ missed_calls_to_email.php
@@ -54,12 +54,12 @@ mysqli_close($link);
 
 if ($txt) {
 
-    $to = "info@bikemall.gr";
+    $to = "info@myemail.com";
     $subject = "Missed calls";
 
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= "From: info@bikemall.gr" . "\r\n";
+    $headers .= "From: info@myemail.com" . "\r\n";
 
     mail($to, $subject, $txt, $headers);
 }
